04624942d786d6d50cbd61f0d912c4b22c838a3e
Ajit kumar <76962571+ajit202120044@users.noreply.github.com>
2026-07-01 00:31:15 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    string helper(string &s){
+         string ans;
+        for (auto it : s){
+            if (isalnum(it)){
+                ans += tolower(it);
+            }
+        }
+        return ans;
+    }
+    bool isPalindrome(string s) {
+           string alum = helper(s);
+           int n = alum.size();
+          int i =0;
+           int j = n-1;
+           bool ans = true;
+        while(i<=j){
+            if (alum[i] == alum[j]){
+                ans = true;
+                i++;
+                j--;
+            }
+            else return false;
+        }
+    return ans;
+        
+    }
+};
