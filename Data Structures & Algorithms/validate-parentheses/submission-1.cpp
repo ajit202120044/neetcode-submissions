2bc9b1c1e61142ab2b7226ff682477a5d12b4c3d
Ajit kumar <76962571+ajit202120044@users.noreply.github.com>
2026-07-08 01:12:03 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+              st.push(s[i]);
+            }
+            else {
+                if (st.empty()) return false;
+               if(!st.empty()) {
+            
+                if ((st.top() == '(' and s[i] != ')') or st.top() == '{' and s[i] != '}' or
+                    st.top() == '[' and s[i] != ']')
+                    return false;
+                    st.pop();
+
+            }
+            }
+            
+        }
+
+return st.empty();
+    }
+};
